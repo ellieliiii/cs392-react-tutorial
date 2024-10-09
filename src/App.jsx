686c360e36833269dcb40d9252c6ca2c03d035c8
Ellie Li <ellieli@Ellies-MacBook-Pro-5.local>
2024-10-08 23:19:23 -0500

    styling with bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,44 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Course from './Course';
 
 const schedule = {
-  "title": "CS Courses for 2018-2019",
-  "courses": {
-    "F101": {
-      "term": "Fall",
-      "number": "101",
-      "meets": "MWF 11:00-11:50",
-      "title": "Computer Science: Concepts, Philosophy, and Connections"
+  title: "CS Courses for 2018-2019",
+  courses: {
+    F101: {
+      term: "Fall",
+      number: "101",
+      meets: "MWF 11:00-11:50",
+      title: "Computer Science: Concepts, Philosophy, and Connections"
     },
-    "F110": {
-      "term": "Fall",
-      "number": "110",
-      "meets": "MWF 10:00-10:50",
-      "title": "Intro Programming for non-majors"
+    F110: {
+      term: "Fall",
+      number: "110",
+      meets: "MWF 10:00-10:50",
+      title: "Intro Programming for non-majors"
     },
-    "S313": {
-      "term": "Spring",
-      "number": "313",
-      "meets": "TuTh 15:30-16:50",
-      "title": "Tangible Interaction Design and Learning"
+    S313: {
+      term: "Spring",
+      number: "313",
+      meets: "TuTh 15:30-16:50",
+      title: "Tangible Interaction Design and Learning"
     },
-    "S314": {
-      "term": "Spring",
-      "number": "314",
-      "meets": "TuTh 9:30-10:50",
-      "title": "Tech & Human Interaction"
+    S314: {
+      term: "Spring",
+      number: "314",
+      meets: "TuTh 9:30-10:50",
+      title: "Tech & Human Interaction"
     }
   }
 };
-
-const Course = ({ id, course }) => (
-  <div className="course">
-    <h2>{course.term} CS {course.number}</h2>
-    <p><strong>{course.title}</strong></p>
-    <p><em>{course.meets}</em></p>
-  </div>
-);
 
 const App = () => {
   const coursesArray = Object.entries(schedule.courses);
 
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="container">
+      <header className="my-4">
         <h1>{schedule.title}</h1>
       </header>
       <div className="course-list">
@@ -51,6 +46,7 @@ const App = () => {
           <Course key={id} id={id} course={course} />
         ))}
       </div>
+
     </div>
   );
 };
---
+++ src/Course.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './Course.css';
+
+const Course = ({ id, course }) => (
+  <div className="col-md-4 mb-4">
+    <div className="card h-100">
+      <div className="card-body">
+        <h5 className="card-title">{course.term} CS {course.number}</h5>
+        <h6 className="card-subtitle mb-2 text-muted">{course.title}</h6>
+        <hr /> {/* Horizontal line above the time */}
+        <p className="card-text">{course.meets}</p>
+      </div>
+    </div>
+  </div>
+);
+
+export default Course;
